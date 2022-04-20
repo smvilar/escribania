068acc47f0316618c998d72b1225adcee14ebb06
Santiago M. Vilar <smvilar@gmail.com>
2022-04-20 11:50:35 -0300

    change base path for gh build

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -9,6 +11,9 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/escribania',
+    },
     prerender: {
       default: true
     }
